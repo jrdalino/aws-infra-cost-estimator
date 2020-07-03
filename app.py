f67e803e8a92433a3af2c1a48f22e269e76c2f7d
Jose Dalino <jrdalino@gmail.com>
2020-07-03 18:59:06 +0800

    Initial

--- app.py
+++ app.py
@@ -6,10 +6,20 @@ client = boto3.client('pricing', region_name='us-east-1')
 
 app = Flask(__name__)
 
-@app.route('/todo/api/v1.0/pricing', methods=['GET'])
-def get_pricing():
+@app.route('/api/v1.0/services', methods=['GET'])
+def describe_services():
+    response = client.describe_services(
+        ServiceCode='AmazonEC2',        
+        FormatVersion='aws_v1',
+        MaxResults=3
+    )
+    return jsonify({'services': response})
+
+@app.route('/api/v1.0/products', methods=['GET'])
+def get_products():
     response = client.get_products(
         ServiceCode='AmazonRDS',
+        FormatVersion='aws_v1',
         Filters=[
             {
                 'Type': 'TERM_MATCH',
@@ -31,10 +41,9 @@ def get_pricing():
                 'Field': 'location',
                 'Value': 'Asia Pacific (Singapore)'
             },
-        ],
-        FormatVersion='aws_v1'
+        ]
     )
-    return jsonify({'pricing': response})
+    return jsonify({'products': response})
 
 if __name__ == '__main__':
     app.run(debug=True)
